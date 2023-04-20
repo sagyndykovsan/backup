f080d9bc842e85118c3ff11a82a686e1bd594300
daniil <daniil.popoff01@gmail.com>
2023-04-20 21:19:18 +0600

    migrations

--- fond_app/admin.py
+++ fond_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from fond_app.models import Video, Book, Presentation
+from fond_app.models import Video, Book, Presentation, QA, Question
 
 admin.site.register(Video)
 admin.site.register(Book)
 admin.site.register(Presentation)
+
+admin.site.register(QA)
+admin.site.register(Question)
