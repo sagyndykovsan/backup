0b8a01cd2a889a9cd13d4b7d7330f50bfa748092
Kydyrbek97 <Kydyrali_097@mail.ru>
2023-04-20 16:48:46 +0600

    added version

--- fond_project/urls.py
+++ fond_project/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('scholarship.urls')),
+    path('', include('fond_app.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
