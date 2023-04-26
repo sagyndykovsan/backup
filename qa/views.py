448b5cec2d523c22ee2412a51226c3a22c0ec1f8
sagyndykovsan <sanjar.sagyndykov@gmail.com>
2023-04-26 14:59:44 +0600

    up

--- qa/views.py
+++ qa/views.py
@@ -1,8 +1,13 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from .models import QA, Question
 
 # Create your views here.
-
-
-def index(request):
-    context = {}
-    return render(request, 'QA.html', context)
+@csrf_exempt
+def qa(request):
+    qas = QA.objects.all()
+    if request.method == 'GET':
+        print(qas)
+        return render(request, "QA.html", {'question': qas})
+    
+    print(request.POST.get('gname'))
