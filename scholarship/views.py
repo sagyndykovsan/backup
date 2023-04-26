be90cbf3784e14ab791e3e8b4f6dffd345b1f6da
Kydyrbek97 <Kydyrali_097@mail.ru>
2023-04-26 13:24:07 +0600

    added detail scholarships

--- scholarship/views.py
+++ scholarship/views.py
@@ -7,6 +7,11 @@ def scholarship(request):
     return render(request, "project.html", {"el": el})
 
 
-def detail(request):
-    d = Scholarship.objects.all()
+def scholarship_name(request):
+    names = Scholarship.objects.all()
+    return render(request, "details.html", {"names": names})
+
+
+def detail(request, pk):
+    d = Scholarship.objects.get(pk=pk)
     return render(request, "details.html", {"d": d})
